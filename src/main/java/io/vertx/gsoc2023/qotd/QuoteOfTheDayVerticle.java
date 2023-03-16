f64e996e32ed8899aba78d8f895084a52ec2d313
Abhishek Das <abhishekd.cst@gmail.com>
2023-03-16 11:45:02 +0530

    1) Added BodyHandler to POST endpoint /quotes 2) Updated POST endpoint /quotes to respond with the newly created quote, also updated the test cases to utilize this response body 3) Updated endpoint /realtime to unregister the evenbus consumer when websocket is closed

--- src/main/java/io/vertx/gsoc2023/qotd/QuoteOfTheDayVerticle.java
+++ src/main/java/io/vertx/gsoc2023/qotd/QuoteOfTheDayVerticle.java
@@ -7,11 +7,13 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.pgclient.PgPool;
 import io.vertx.sqlclient.PoolOptions;
@@ -47,6 +49,7 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
   private Router setupRouter() {
     Router router = Router.router(vertx);
     final String REALTIME_EVENT_ADDRESS = "realtime-quote";
+    final String CONTENT_TYPE_JSON = "application/json";
 
     router.route().failureHandler(ctx -> {
       ctx.response()
@@ -54,54 +57,62 @@ public class QuoteOfTheDayVerticle extends AbstractVerticle {
         .end("Failure: " + ctx.failure().getMessage());
     });
 
-    router.get("/quotes").handler(ctx -> {
+    router.get("/quotes")
+    .produces(CONTENT_TYPE_JSON)
+    .handler(ctx -> {
       selectQuotes().compose(rows -> {
         JsonArray jsonArray = new JsonArray();
         rows.forEach(row -> jsonArray.add(row.toJson()));
 
         return ctx.response()
-          .putHeader(HttpHeaderNames.CONTENT_TYPE, "application/json")
+          .putHeader(HttpHeaderNames.CONTENT_TYPE, CONTENT_TYPE_JSON)
           .end(jsonArray.encodePrettily());
       }).onFailure(cause -> ctx.fail(cause));
     });
 
-    router.post("/quotes").handler(ctx -> {
-      ctx.request()
-        .body()
-        .compose(buff -> {
-          JsonObject jsonObject = buff.toJsonObject();
+    router.post("/quotes")
+    .consumes(CONTENT_TYPE_JSON)
+    .produces(CONTENT_TYPE_JSON)
+    .handler(BodyHandler.create(false).setBodyLimit(100 * 1024))
+    .handler(ctx -> {
+      JsonObject jsonObject = ctx.body().asJsonObject();
   
-          if (jsonObject.getString("text") == null) {
-            return ctx.response()
-              .setStatusCode(HttpResponseStatus.BAD_REQUEST.code())
-              .end("Quote text must be provided in payload");
-          }
-  
-          return insertQuoteAndReturn(jsonObject.getString("author", "Unknown"), jsonObject.getString("text"))
-              .compose(rows -> {
-                JsonObject insertedQuote = rows.iterator().next().toJson();
+      if (jsonObject.getString("text") == null) {
+        ctx.response()
+          .setStatusCode(HttpResponseStatus.BAD_REQUEST.code())
+          .end("Quote text must be provided in payload");
+          return;
+      }
 
-                ctx.vertx()
-                  .eventBus()
-                  .publish(REALTIME_EVENT_ADDRESS, insertedQuote, new DeliveryOptions().setLocalOnly(true));
-              
-                return ctx.response()
-                  .setStatusCode(HttpResponseStatus.CREATED.code())
-                  .putHeader(HttpHeaderNames.CONTENT_TYPE, "application/json")
-                  .end(insertedQuote.encodePrettily());
-              });
-        }).onFailure(cause -> ctx.fail(cause));
+      insertQuoteAndReturn(jsonObject.getString("author", "Unknown"), jsonObject.getString("text"))
+      .compose(rows -> {
+        JsonObject insertedQuote = rows.iterator().next().toJson();
+
+        ctx.vertx()
+          .eventBus()
+          .publish(REALTIME_EVENT_ADDRESS, insertedQuote, new DeliveryOptions().setLocalOnly(true));
+      
+        return ctx
+          .response()
+          .setStatusCode(HttpResponseStatus.CREATED.code())
+          .putHeader(HttpHeaderNames.CONTENT_TYPE, CONTENT_TYPE_JSON)
+          .end(insertedQuote.encodePrettily());
+      }).onFailure(cause -> ctx.fail(cause));
     });
 
     router.get("/realtime").handler(ctx -> {
       ctx.request()
         .toWebSocket()
         .onSuccess(webSocket -> {
-          ctx.vertx()
+          MessageConsumer<JsonObject> consumer = ctx.vertx()
             .eventBus()
             .localConsumer(REALTIME_EVENT_ADDRESS, message -> {
               webSocket.writeTextMessage(message.body().toString());
             });
+          
+          webSocket.closeHandler(v -> {
+            consumer.unregister();
+          });
         }).onFailure(cause -> ctx.fail(cause));
     });
 
--- src/test/java/io/vertx/gsoc2023/qotd/QuoteOfTheDayVerticleTest.java
+++ src/test/java/io/vertx/gsoc2023/qotd/QuoteOfTheDayVerticleTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.testcontainers.containers.BindMode.READ_ONLY;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,7 +23,6 @@ import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.ext.web.client.predicate.ResponsePredicate;
 import io.vertx.ext.web.codec.BodyCodec;
-import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 
@@ -73,7 +69,6 @@ public class QuoteOfTheDayVerticleTest {
       .expect(ResponsePredicate.JSON)
       .send(testContext.succeeding(response -> {
         testContext.verify(() -> {
-          assertEquals(200, response.statusCode(), response.bodyAsString());
           JsonArray quotes = response.body();
           assertFalse(quotes.isEmpty());
           testContext.completeNow();
@@ -86,38 +81,18 @@ public class QuoteOfTheDayVerticleTest {
     JsonObject testQuote = new JsonObject()
       .put("author", "Francis Bacon")
       .put("text", "Knowledge is power");
-  
-    Checkpoint postQuoteCheck = testContext.checkpoint();
-    Checkpoint getQuoteCheck = testContext.checkpoint();
-    
+      
     webClient.post("/quotes")
+      .as(BodyCodec.jsonObject())
       .expect(ResponsePredicate.SC_CREATED)
+      .expect(ResponsePredicate.JSON)
       .sendJsonObject(testQuote, testContext.succeeding(postResponse -> {
         testContext.verify(() -> {
-          assertEquals(201, postResponse.statusCode(), postResponse.bodyAsString());
-          postQuoteCheck.flag();
+          JsonObject createdQuote = postResponse.body();
+          assertEquals(testQuote.getString("author"), createdQuote.getString("author"));
+          assertEquals(testQuote.getString("text"), createdQuote.getString("text"));
+          testContext.completeNow();
         });
-        
-        webClient.get("/quotes")
-          .as(BodyCodec.jsonArray())
-          .expect(ResponsePredicate.SC_OK)
-          .expect(ResponsePredicate.JSON)
-          .send(testContext.succeeding(getResponse -> {
-            testContext.verify(() -> {
-              assertEquals(200, getResponse.statusCode(), postResponse.bodyAsString());
-              JsonArray quotes = getResponse.body();
-              
-              List<JsonObject> quoteList = quotes.stream()
-                .map(obj -> (JsonObject) obj)
-                .filter(quote -> 
-                  (testQuote.getString("author").equals(quote.getString("author"))
-                      && testQuote.getString("text").equals(quote.getString("text"))))
-                .collect(Collectors.toList());
-              
-              assertFalse(quoteList.isEmpty());
-              getQuoteCheck.flag();
-            });
-        }));
       }));
   }
   
@@ -125,38 +100,18 @@ public class QuoteOfTheDayVerticleTest {
   public void testPostQuoteWithoutAuthor(VertxTestContext testContext) {
     JsonObject testQuote = new JsonObject()
       .put("text", "Knowledge is power");
-  
-    Checkpoint postQuoteCheck = testContext.checkpoint();
-    Checkpoint getQuoteCheck = testContext.checkpoint();
-    
+      
     webClient.post("/quotes")
+      .as(BodyCodec.jsonObject())
       .expect(ResponsePredicate.SC_CREATED)
+      .expect(ResponsePredicate.JSON)
       .sendJsonObject(testQuote, testContext.succeeding(postResponse -> {
         testContext.verify(() -> {
-          assertEquals(201, postResponse.statusCode(), postResponse.bodyAsString());
-          postQuoteCheck.flag();
+          JsonObject createdQuote = postResponse.body();
+          assertEquals("Unknown", createdQuote.getString("author"));
+          assertEquals(testQuote.getString("text"), createdQuote.getString("text"));
+          testContext.completeNow();
         });
-        
-        webClient.get("/quotes")
-          .as(BodyCodec.jsonArray())
-          .expect(ResponsePredicate.SC_OK)
-          .expect(ResponsePredicate.JSON)
-          .send(testContext.succeeding(getResponse -> {
-            testContext.verify(() -> {
-              assertEquals(200, getResponse.statusCode(), postResponse.bodyAsString());
-              JsonArray quotes = getResponse.body();
-              
-              List<JsonObject> quoteList = quotes.stream()
-                .map(obj -> (JsonObject) obj)
-                .filter(quote -> 
-                  ("Unknown".equals(quote.getString("author"))
-                      && testQuote.getString("text").equals(quote.getString("text"))))
-                .collect(Collectors.toList());
-              
-              assertFalse(quoteList.isEmpty());
-              getQuoteCheck.flag();
-            });
-        }));
       }));
   }
   
@@ -167,12 +122,8 @@ public class QuoteOfTheDayVerticleTest {
       
     webClient.post("/quotes")
       .expect(ResponsePredicate.SC_BAD_REQUEST)
-      .sendJsonObject(testQuote, testContext.succeeding(postResponse -> {
-        testContext.verify(() -> {
-          assertEquals(400, postResponse.statusCode(), postResponse.bodyAsString());
-          testContext.completeNow();
-        });
-      }));
+      .sendJsonObject(testQuote, testContext.succeeding(response -> testContext.completeNow()));
+
   }
   
   @Test
