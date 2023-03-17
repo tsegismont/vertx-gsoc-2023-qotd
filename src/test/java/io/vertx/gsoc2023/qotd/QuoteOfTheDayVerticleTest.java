85866e167dba593f701842bbcfe23b2e71277434
Denis Julio <djms1234@gmail.com>
2023-03-17 17:06:59 -0300

    Add tests to handle scenarios where there is no connection to the container

--- src/test/java/io/vertx/gsoc2023/qotd/QuoteOfTheDayVerticleTest.java
+++ src/test/java/io/vertx/gsoc2023/qotd/QuoteOfTheDayVerticleTest.java
@@ -44,163 +44,202 @@ public class QuoteOfTheDayVerticleTest {
     .withClasspathResourceMapping("import.sql", "/docker-entrypoint-initdb.d/import.sql", READ_ONLY)
     .withExposedPorts(5432);
 
-  @BeforeEach
-  public void setup(VertxTestContext testContext) {
-    JsonObject config = new JsonObject()
-      .put("dbUser", "quotes")
-      .put("dbPassword", "super$ecret")
-      .put("dbName", "quotes")
-      .put("dbPort", postgres.getMappedPort(5432))
-      .put("httpPort", PORT);
-    DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(config);
-    vertx.deployVerticle(new QuoteOfTheDayVerticle(), deploymentOptions, testContext.succeedingThenComplete());
-  }
+  @Nested
+  class WhenConnectingToContainer {
 
-  @AfterEach
-  public void tearDown(VertxTestContext testContext) {
-    vertx.close(testContext.succeedingThenComplete());
-  }
+    @BeforeEach
+    public void setup(VertxTestContext testContext) {
+      JsonObject config = new JsonObject()
+        .put("dbUser", "quotes")
+        .put("dbPassword", "super$ecret")
+        .put("dbName", "quotes")
+        .put("dbPort", postgres.getMappedPort(5432))
+        .put("httpPort", PORT);
+      DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(config);
+      vertx.deployVerticle(new QuoteOfTheDayVerticle(), deploymentOptions, testContext.succeedingThenComplete());
+    }
 
-  @Test
-  public void testGetQuotes(VertxTestContext testContext) {
-    webClient.get("/quotes")
-      .as(BodyCodec.jsonArray())
-      .expect(ResponsePredicate.SC_OK)
-      .expect(ResponsePredicate.JSON)
-      .send(testContext.succeeding(response ->
-        testContext.verify(() -> {
-          assertEquals(200, response.statusCode(), response.bodyAsString());
-          JsonArray quotes = response.body();
-          assertFalse(quotes.isEmpty());
-          testContext.completeNow();
-        })));
-  }
+    @AfterEach
+    public void tearDown(VertxTestContext testContext) {
+      vertx.close(testContext.succeedingThenComplete());
+    }
 
-  @Test
-  public void testNewQuoteWithAuthor(VertxTestContext testContext) {
-    var payload = new JsonObject()
-      .put("text", "Have you tried double-clicking the icon?")
-      .put("author", "Jen Barber");
-    var postRequest = webClient.post("/quotes")
-      .timeout(5000)
-      .expect(ResponsePredicate.SC_CREATED)
-      .sendJsonObject(payload);
-    testContext.assertComplete(postRequest)
-      .onComplete(__ -> {
-        webClient.get("/quotes")
-          .as(BodyCodec.jsonArray())
-          .send(testContext.succeeding(response ->
-            testContext.verify(() -> {
-              var size = response.body().size();
-              var insertedQuote = response.body().getJsonObject(size - 1);
-              assertEquals("Jen Barber", insertedQuote.getString("author"));
-              testContext.completeNow();
-            })));
-      });
-  }
+    @Test
+    public void testGetQuotes(VertxTestContext testContext) {
+      webClient.get("/quotes")
+        .as(BodyCodec.jsonArray())
+        .expect(ResponsePredicate.SC_OK)
+        .expect(ResponsePredicate.JSON)
+        .send(testContext.succeeding(response ->
+          testContext.verify(() -> {
+            assertEquals(200, response.statusCode(), response.bodyAsString());
+            JsonArray quotes = response.body();
+            assertFalse(quotes.isEmpty());
+            testContext.completeNow();
+          })));
+    }
 
-  @Test
-  public void testNewQuoteWithoutAuthor(VertxTestContext testContext) {
-    var payload = new JsonObject()
-      .put("text", "Life is too short.");
-    var postRequest = webClient.post("/quotes")
-      .timeout(5000)
-      .expect(ResponsePredicate.SC_CREATED)
-      .sendJsonObject(payload);
-    testContext.assertComplete(postRequest)
-      .onComplete(__ -> {
-        webClient.get("/quotes")
-          .as(BodyCodec.jsonArray())
-          .send(testContext.succeeding(response ->
-            testContext.verify(() -> {
-              var size = response.body().size();
-              var insertedQuote = response.body().getJsonObject(size - 1);
-              assertEquals("Unknown", insertedQuote.getString("author"));
-              testContext.completeNow();
-            })));
-      });
-  }
+    @Test
+    public void testNewQuoteWithAuthor(VertxTestContext testContext) {
+      var payload = new JsonObject()
+        .put("text", "Have you tried double-clicking the icon?")
+        .put("author", "Jen Barber");
+      var postRequest = webClient.post("/quotes")
+        .timeout(5000)
+        .expect(ResponsePredicate.SC_CREATED)
+        .sendJsonObject(payload);
+      testContext.assertComplete(postRequest)
+        .onComplete(__ -> {
+          webClient.get("/quotes")
+            .as(BodyCodec.jsonArray())
+            .send(testContext.succeeding(response ->
+              testContext.verify(() -> {
+                var size = response.body().size();
+                var insertedQuote = response.body().getJsonObject(size - 1);
+                assertEquals("Jen Barber", insertedQuote.getString("author"));
+                testContext.completeNow();
+              })));
+        });
+    }
 
-  @Test
-  public void testNewQuoteMissingText(VertxTestContext testContext) {
-    var payload = new JsonObject()
-      .put("author", "Camus");
-    webClient.post("/quotes")
-      .timeout(5000)
-      .expect(ResponsePredicate.SC_BAD_REQUEST)
-      .sendJsonObject(payload, testContext.succeeding(response ->
-        testContext.verify(() -> {
-          assertEquals(400, response.statusCode());
-          testContext.completeNow();
-        })));
+    @Test
+    public void testNewQuoteWithoutAuthor(VertxTestContext testContext) {
+      var payload = new JsonObject()
+        .put("text", "Life is too short.");
+      var postRequest = webClient.post("/quotes")
+        .timeout(5000)
+        .expect(ResponsePredicate.SC_CREATED)
+        .sendJsonObject(payload);
+      testContext.assertComplete(postRequest)
+        .onComplete(__ -> {
+          webClient.get("/quotes")
+            .as(BodyCodec.jsonArray())
+            .send(testContext.succeeding(response ->
+              testContext.verify(() -> {
+                var size = response.body().size();
+                var insertedQuote = response.body().getJsonObject(size - 1);
+                assertEquals("Unknown", insertedQuote.getString("author"));
+                testContext.completeNow();
+              })));
+        });
+    }
+
+    @Test
+    public void testNewQuoteMissingText(VertxTestContext testContext) {
+      var payload = new JsonObject()
+        .put("author", "Camus");
+      webClient.post("/quotes")
+        .timeout(5000)
+        .expect(ResponsePredicate.SC_BAD_REQUEST)
+        .sendJsonObject(payload, testContext.succeeding(response ->
+          testContext.verify(() -> {
+            assertEquals(400, response.statusCode());
+            testContext.completeNow();
+          })));
+    }
+
+    @Nested
+    class RealtimeTest {
+
+      private WebSocketConnectOptions wsOptions;
+
+      @BeforeEach
+      void setup() {
+        wsOptions = new WebSocketConnectOptions()
+          .setTimeout(5000)
+          .setHost("localhost")
+          .setURI("/realtime");
+      }
+
+      @Test
+      public void testRealtimeQuotes(VertxTestContext testContext) {
+        var postRequestSent = testContext.checkpoint();
+        var connectedToWs = testContext.checkpoint();
+        var receivedData = testContext.checkpoint();
+        httpClient
+          .webSocket(wsOptions)
+          .onSuccess(ws -> {
+            var postQuote = new JsonObject()
+              .put("text", "Vert.x is the state of the art in the reactive tooling for Java")
+              .put("author", "Denis Julio");
+            webClient.post("/quotes")
+              .timeout(5000)
+              .sendJsonObject(postQuote)
+              .onSuccess(__ -> postRequestSent.flag());
+            connectedToWs.flag();
+          })
+          .onComplete(testContext.succeeding(ws ->
+            ws.binaryMessageHandler(buffer ->
+              testContext.verify(() -> {
+                var newQuote = buffer.toJsonObject();
+                assertNotNull(newQuote.getString("quote_id"));
+                assertEquals("Denis Julio", newQuote.getString("author"));
+                ws.close();
+                receivedData.flag();
+              }))));
+      }
+
+      @Test
+      public void testRealtimeQuotesWithouAuthor(VertxTestContext testContext) {
+        var postRequestSent = testContext.checkpoint();
+        var connectedToWs = testContext.checkpoint();
+        var receivedData = testContext.checkpoint();
+        httpClient
+          .webSocket(wsOptions)
+          .onSuccess(ws -> {
+            var postQuote = new JsonObject()
+              .put("text", "You shall not PASS!!!");
+            webClient.post("/quotes")
+              .timeout(5000)
+              .sendJsonObject(postQuote)
+              .onSuccess(__ -> postRequestSent.flag());
+            connectedToWs.flag();
+          })
+          .onComplete(testContext.succeeding(ws ->
+            ws.binaryMessageHandler(buffer ->
+              testContext.verify(() -> {
+                var newQuote = buffer.toJsonObject();
+                assertNotNull(newQuote.getString("quote_id"));
+                assertEquals("Unknown", newQuote.getString("author"));
+                ws.close();
+                receivedData.flag();
+              }))));
+      }
+    }
   }
 
   @Nested
-  class RealtimeTest {
-
-    private WebSocketConnectOptions wsOptions;
+  class WhenFailToConnectToContainer {
 
     @BeforeEach
-    void setup() {
-      wsOptions = new WebSocketConnectOptions()
-        .setTimeout(5000)
-        .setHost("localhost")
-        .setURI("/realtime");
+    public void setup(VertxTestContext testContext) {
+      JsonObject config = new JsonObject()
+        .put("dbUser", "quotes")
+        .put("dbPassword", "super")
+        .put("dbName", "quotes")
+        .put("dbPort", postgres.getMappedPort(5432))
+        .put("httpPort", PORT);
+      DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(config);
+      vertx.deployVerticle(new QuoteOfTheDayVerticle(), deploymentOptions, testContext.succeedingThenComplete());
     }
 
     @Test
-    public void testRealtimeQuotes(VertxTestContext testContext) {
-      var postRequestSent = testContext.checkpoint();
-      var connectedToWs = testContext.checkpoint();
-      var receivedData = testContext.checkpoint();
-      httpClient
-        .webSocket(wsOptions)
-        .onSuccess(ws -> {
-          var postQuote = new JsonObject()
-            .put("text", "Vert.x is the state of the art in the reactive tooling for Java")
-            .put("author", "Denis Julio");
-          webClient.post("/quotes")
-            .timeout(5000)
-            .sendJsonObject(postQuote)
-            .onSuccess(__ -> postRequestSent.flag());
-          connectedToWs.flag();
-        })
-        .onComplete(testContext.succeeding(ws ->
-          ws.binaryMessageHandler(buffer ->
+    public void testGetQuotesHandleError(VertxTestContext testContext) {
+      webClient.get("/quotes")
+        .as(BodyCodec.jsonObject())
+        .expect(ResponsePredicate.SC_INTERNAL_SERVER_ERROR)
+        .send(testContext.succeeding(response -> {
             testContext.verify(() -> {
-              var newQuote = buffer.toJsonObject();
-              assertNotNull(newQuote.getString("quote_id"));
-              assertEquals("Denis Julio", newQuote.getString("author"));
-              ws.close();
-              receivedData.flag();
-            }))));
+              assertNotNull(response.body().getString("message"));
+              testContext.completeNow();
+            });
+          })
+        );
     }
 
-    @Test
-    public void testRealtimeQuotesWithouAuthor(VertxTestContext testContext) {
-      var postRequestSent = testContext.checkpoint();
-      var connectedToWs = testContext.checkpoint();
-      var receivedData = testContext.checkpoint();
-      httpClient
-        .webSocket(wsOptions)
-        .onSuccess(ws -> {
-          var postQuote = new JsonObject()
-            .put("text", "You shall not PASS!!!");
-          webClient.post("/quotes")
-            .timeout(5000)
-            .sendJsonObject(postQuote)
-            .onSuccess(__ -> postRequestSent.flag());
-          connectedToWs.flag();
-        })
-        .onComplete(testContext.succeeding(ws ->
-          ws.binaryMessageHandler(buffer ->
-            testContext.verify(() -> {
-              var newQuote = buffer.toJsonObject();
-              assertNotNull(newQuote.getString("quote_id"));
-              assertEquals("Unknown", newQuote.getString("author"));
-              ws.close();
-              receivedData.flag();
-            }))));
+    @AfterEach
+    public void tearDown(VertxTestContext testContext) {
+      vertx.close(testContext.succeedingThenComplete());
     }
   }
 }
